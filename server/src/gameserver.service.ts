81965046193e51e0f0f94a399046d2216fc9188b
emenella <emenella@student.42lyon.fr>
2022-12-21 21:51:45 +0100

    Remove Server de la mort

--- server/src/gameserver.service.ts
+++
@@ -1,8 +0,0 @@
-import { Injectable } from "@nestjs/common";
-import { GameController } from "./gameserver.controller";
-
-@Injectable()
-export class GameService
-{
-
-}
--- server/src/main.ts
+++
@@ -1,8 +0,0 @@
-import { NestFactory } from '@nestjs/core';
-import { GameModule } from './gameserver.module'
-
-async function bootstrap() {
-  const app = await NestFactory.create(GameModule);
-  await app.listen(3000);
-}
-bootstrap();
